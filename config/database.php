fb684eca64591d01f37dfb3c798465774340cb08
Antonio Silva <antoniomarcos.silva@protonmail.com>
2026-04-30 16:24:10 -0400

    docs: add database.php (con)

---
+++ config/database.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'connection' => 'mysql'
+    'host' => 'mysql',
+    'port' => '3306'
+    'database' => 'api_pro',
+    'username' => 'XxZeroxX',
+    'password' => 'XxZero123xX',
+    'charset' => 'utf8mb4',
+];
